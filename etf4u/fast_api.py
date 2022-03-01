86c1eb47647cdbdd18f67734f20fef59c9b48302
ohjho <jho.yvr+git@gmail.com>
2022-03-01 11:02:31 +0800

    updated caching to do timed_lru_cache

--- etf4u/fast_api.py
+++ etf4u/fast_api.py
@@ -13,27 +13,52 @@ app = FastAPI(
 	version = app_version
 )
 
-@app.get("/query", response_model = dict, response_model_exclude_unset = True)
-async def decompose(etf_ticker: str, do_cache: bool = True):
-    ''' get ETF constituents
-    * `etf_ticker`: a single etf symbol/ ticker
-    * `do_cache`: utilize daily caching for faster response time
-    '''
+# For Caching
+from functools import lru_cache, wraps
+from datetime import datetime, timedelta
+
+def timed_lru_cache(seconds: int, maxsize: int = 128):
+	''' add an expiry to the lru_cache
+	ref: https://realpython.com/lru-cache-python/
+	'''
+	def wrapper_cache(func):
+		func = lru_cache(maxsize=maxsize)(func)
+		func.lifetime = timedelta(seconds=seconds)
+		func.expiration = datetime.utcnow() + func.lifetime
+
+		@wraps(func)
+		def wrapped_func(*args, **kwargs):
+			if datetime.utcnow() >= func.expiration:
+				func.cache_clear()
+				func.expiration = datetime.utcnow() + func.lifetime
+			return func(*args, **kwargs)
+
+		return wrapped_func
+
+	return wrapper_cache
+
+@timed_lru_cache(seconds = 8*60**2) #cache data for 8 hours
+def decompose(etf_ticker):
     sanitized_fund = etf_ticker.lower()
     result = None
     for loader, name, _ in pkgutil.iter_modules(adapters.__path__):
         adapter = loader.find_module(name).load_module(name)
         if sanitized_fund in [f.lower() for f in adapter.FUNDS]:
             # log.info(f"Fetching ETF {sanitized_fund.upper()} using {name} adapter")
-            result = query(sanitized_fund, adapter.fetch) if do_cache else \
-                        adapter.fetch(sanitized_fund)
+            result = adapter.fetch(sanitized_fund)
             break
     if not result:
         # log.warning(f"No adapter found for ETF {fund}, using default etfdbd adapter")
         from adapters import etfdb
-        result = query(sanitized_fund, etfdb.fetch) if do_cache else \
-                    etfdb.fetch(sanitized_fund)
+        result = etfdb.fetch(sanitized_fund)
     return {k: result[k] for k in sorted(result, key = result.get, reverse= True)}
+
+@app.get("/query", response_model = dict, response_model_exclude_unset = True)
+async def query(etf_ticker: str):
+    ''' get ETF constituents
+    * `etf_ticker`: a single etf symbol/ ticker
+    '''
+    return decompose(etf_ticker)
 
 @app.get('/')
 def read_root():
