3d54498540d60446c871bd2e1dff89a16fe0388e
John Ho <jho.yvr+git@gmail.com>
2024-09-10 10:37:44 -0400

    updated ark's URLs

--- etf4u/adapters/ark.py
+++ etf4u/adapters/ark.py
@@ -5,19 +5,22 @@ log = logging.getLogger(f"etf4u.{__name__}")
 
 # The ARK adapter fetches the .csv file of the funds' holdings published on their site
 
-FUNDS = ["arkk", "arkw", "arkq", "arkf", "arkg"]
+FUNDS = ["arkk", "arkw", "arkq", "arkf", "arkg", "arkx", "prnt", "izrl"]
 
 
 def get_fund_file(fund):
     funds_filenames = {
         "arkk": "ARK_INNOVATION_ETF_ARKK_HOLDINGS",
-        "arkw": "ARK_INNOVATION_ETF_ARKW_HOLDINGS",
-        "arkq": "ARK_INNOVATION_ETF_ARKQ_HOLDINGS",
-        "arkf": "ARK_INNOVATION_ETF_ARKF_HOLDINGS",
-        "arkg": "ARK_INNOVATION_ETF_ARKG_HOLDINGS"
+        "arkw": "ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS",
+        "arkq": "ARK_AUTONOMOUS_TECH._&_ROBOTICS_ETF_ARKQ_HOLDINGS",
+        "arkf": "ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS",
+        "arkg": "ARK_GENOMIC_REVOLUTION_ETF_ARKG_HOLDINGS",
+        "arkx": "ARK_SPACE_EXPLORATION_&_INNOVATION_ETF_ARKX_HOLDINGS",
+        "prnt": "THE_3D_PRINTING_ETF_PRNT_HOLDINGS",
+        "izrl": "ARK_ISRAEL_INNOVATIVE_TECHNOLOGY_ETF_IZRL_HOLDINGS",
     }
     return (
-        "https://ark-funds.com/wp-content/uploads/funds-etf-csv/"
+        "https://assets.ark-funds.com/fund-documents/funds-etf-csv/"
         + funds_filenames[fund]
         + ".csv"
     )
@@ -36,7 +39,7 @@ def fetch(fund):
             weight = holding[7]
             if not ticker or not weight:
                 continue
-            result[ticker] = result.get(ticker, 0) + float(weight.strip('%')) #/100
+            result[ticker] = result.get(ticker, 0) + float(weight.strip("%"))  # /100
         except IndexError:
             continue
     return result
